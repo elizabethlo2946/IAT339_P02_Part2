56e8ad38343557f3888b096499df8849b10bf143
Unknown <abela@sfu.ca>
2018-03-01 15:22:24 -0800

    Finalized multi-product row interactions

--- js/hoverDescripCollapse.js
+++ js/hoverDescripCollapse.js
@@ -1,90 +1,93 @@
-// START: taken from IAT339 Week7 Lecture
-"use strict";
-console.log("JavaScript is running.");
-
-var width = window.innerWidth;
-console.log("Window width: "+width+"px");
-
-var overlayProductDescriptionBlock = document.querySelectorAll(".overlayProductDescriptionBlock");
-var overlayProductDescriptionHover = document.querySelectorAll(".overlayProductDescriptionHover");
-
-
-window.onresize = toggleHover;
-window.onload = toggleHover;
-
-function toggleHover() {
-	// width = window.innerWidth;
-
-	if (width > 1071) {
-		console.log("Window width is less than 1071px, collapsing menu.");
-
-		 // overlayProductDescriptionHover.classList.remove("hidden");
-		 // // overlayProductDescriptionHover.setAttribute("aria-hidden", "false");
-		 // overlayProductDescriptionBlock.classList.add("hidden");
-		 // // overlayProductDescriptionBlock.setAttribute("aria-hidden", "true");
-		 // // overlayProductDescriptionBlock.setAttribute("aria-labelledby", "overlayProductDescriptionHover");
-
-		 $('.overlayProductDescriptionHover').removeClass("hidden");
-		 // overlayProductDescriptionHover.setAttribute("aria-hidden", "false");
-		 $('.overlayProductDescriptionBlock').addClass("hidden");
-		 // overlayProductDescriptionBlock.setAttribute("aria-hidden", "true");
-		 // overlayProductDescriptionBlock.setAttribute("aria-labelledby", "overlayProductDescriptionHover");
-
-	} else {
-
-		// overlayProductDescriptionHover.classList.add("hidden");
-		// // overlayProductDescriptionHover.setAttribute("aria-hidden", "true");
-		// overlayProductDescriptionBlock.classList.remove("hidden");
-		// // overlayProductDescriptionBlock.setAttribute("aria-hidden", "false");
-
-		$('.overlayProductDescriptionHover').addClass("hidden");
-		// overlayProductDescriptionHover.setAttribute("aria-hidden", "false");
-		$('.overlayProductDescriptionBlock').removeClass("hidden");
-		// overlayProductDescriptionBlock.setAttribute("aria-hidden", "true");
-		// overlayProductDescriptionBlock.setAttribute("aria-labelledby", "overlayProductDescriptionHover");
-	}
-}
-
-// window.addEventListener("resize",
-// 	function() {
-// 		// width = window.innerWidth;
+// // START: taken from IAT339 Week7 Lecture
+// "use strict";
+// console.log("JavaScript is running.");
 //
-// 		if (width > 1071) {
-// 			console.log("Window width is less than 1071px, collapsing menu.");
+// var width = window.innerWidth;
+// console.log("Window width: "+width+"px");
 //
-// 			 overlayProductDescriptionHover.classList.remove("hidden");
-// 			 // overlayProductDescriptionHover.setAttribute("aria-hidden", "false");
-// 			 overlayProductDescriptionBlock.classList.add("hidden");
-// 			 // overlayProductDescriptionBlock.setAttribute("aria-hidden", "true");
-// 			 // overlayProductDescriptionBlock.setAttribute("aria-labelledby", "overlayProductDescriptionHover");
+// var overlayProductDescriptionBlock = document.querySelectorAll(".overlayProductDescriptionBlock");
+// var overlayProductDescriptionHover = document.querySelectorAll(".overlayProductDescriptionHover");
 //
-// 		} else {
 //
-// 			overlayProductDescriptionHover.classList.add("hidden");
-// 			// overlayProductDescriptionHover.setAttribute("aria-hidden", "true");
-// 			overlayProductDescriptionBlock.classList.remove("hidden");
-// 			// overlayProductDescriptionBlock.setAttribute("aria-hidden", "false");
-// 		}
+// window.onresize = toggleHover;
+// window.onload = toggleHover;
+//
+// function toggleHover() {
+// 	// width = window.innerWidth;
+// 	for (i = 0; i < overlayProductDescriptionBlock.size(); i++) {
+//
+//
+// 	if (width > 1071) {
+// 		console.log("Window width is less than 1071px, collapsing menu.");
+//
+// 		 overlayProductDescriptionHover[i].classList.remove("hidden");
+// 		 // overlayProductDescriptionHover.setAttribute("aria-hidden", "false");
+// 		 overlayProductDescriptionBlock[i].classList.add("hidden");
+// 		 // overlayProductDescriptionBlock.setAttribute("aria-hidden", "true");
+// 		 // overlayProductDescriptionBlock.setAttribute("aria-labelledby", "overlayProductDescriptionHover");
+//
+// 		 // $("overlayProductDescriptionHover").removeClass("hidden");
+// 		 // // overlayProductDescriptionHover.setAttribute("aria-hidden", "false");
+// 		 // $("overlayProductDescriptionBlock").addClass("hidden");
+// 		 // // overlayProductDescriptionBlock.setAttribute("aria-hidden", "true");
+// 		 // // overlayProductDescriptionBlock.setAttribute("aria-labelledby", "overlayProductDescriptionHover");
+//
+// 	} else {
+//
+// 		overlayProductDescriptionHover[i].classList.add("hidden");
+// 		// overlayProductDescriptionHover.setAttribute("aria-hidden", "true");
+// 		overlayProductDescriptionBlock[i].classList.remove("hidden");
+// 		// overlayProductDescriptionBlock.setAttribute("aria-hidden", "false");
+//
+// 		// $("overlayProductDescriptionHover").addClass("hidden");
+// 		// // overlayProductDescriptionHover.setAttribute("aria-hidden", "false");
+// 		// $("overlayProductDescriptionBlock").removeClass("hidden");
+// 		// // overlayProductDescriptionBlock.setAttribute("aria-hidden", "true");
+// 		// // overlayProductDescriptionBlock.setAttribute("aria-labelledby", "overlayProductDescriptionHover");
 // 	}
-// );
-
-// navToggle.addEventListener("click",
-//    function() {
-// 		console.log("#nav-toggle has been clicked.");
-//       if ( navItems.classList.contains("hidden") ) {
-// 			console.log("#nav-items are hidden, showing them now.");
+// }
+// }
 //
-// 			navItems.classList.remove("hidden");
-//          navItems.setAttribute("aria-hidden", "false");
-// 			navToggle.setAttribute("aria-expanded", "true");
-//          navFirstItem.focus();
-//          } else {
-// 			console.log("#nav-items are showing, hiding them now.");
+// // window.addEventListener("resize",
+// // 	function() {
+// // 		// width = window.innerWidth;
+// //
+// // 		if (width > 1071) {
+// // 			console.log("Window width is less than 1071px, collapsing menu.");
+// //
+// // 			 overlayProductDescriptionHover.classList.remove("hidden");
+// // 			 // overlayProductDescriptionHover.setAttribute("aria-hidden", "false");
+// // 			 overlayProductDescriptionBlock.classList.add("hidden");
+// // 			 // overlayProductDescriptionBlock.setAttribute("aria-hidden", "true");
+// // 			 // overlayProductDescriptionBlock.setAttribute("aria-labelledby", "overlayProductDescriptionHover");
+// //
+// // 		} else {
+// //
+// // 			overlayProductDescriptionHover.classList.add("hidden");
+// // 			// overlayProductDescriptionHover.setAttribute("aria-hidden", "true");
+// // 			overlayProductDescriptionBlock.classList.remove("hidden");
+// // 			// overlayProductDescriptionBlock.setAttribute("aria-hidden", "false");
+// // 		}
+// // 	}
+// // );
 //
-//          navItems.classList.add("hidden");
-//          navItems.setAttribute("aria-hidden", "true");
-// 			navToggle.setAttribute("aria-expanded", "false");
-//       }
-//    }
-// );
-// END: taken from IAT339 Week7 Lecture
+// // navToggle.addEventListener("click",
+// //    function() {
+// // 		console.log("#nav-toggle has been clicked.");
+// //       if ( navItems.classList.contains("hidden") ) {
+// // 			console.log("#nav-items are hidden, showing them now.");
+// //
+// // 			navItems.classList.remove("hidden");
+// //          navItems.setAttribute("aria-hidden", "false");
+// // 			navToggle.setAttribute("aria-expanded", "true");
+// //          navFirstItem.focus();
+// //          } else {
+// // 			console.log("#nav-items are showing, hiding them now.");
+// //
+// //          navItems.classList.add("hidden");
+// //          navItems.setAttribute("aria-hidden", "true");
+// // 			navToggle.setAttribute("aria-expanded", "false");
+// //       }
+// //    }
+// // );
+// // END: taken from IAT339 Week7 Lecture
